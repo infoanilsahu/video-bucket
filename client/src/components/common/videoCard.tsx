478f1021149236d9dc5ceefbd8156e1ee790a54f
infoanilsahu <info.anilsahu@gmail.com>
2026-06-13 18:19:49 +0530

    feat(frontend): add of home page route

---
+++ client/src/components/common/videoCard.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router";
+
+export function VideoCart( {thumbnailUrl, userProfilePicture, title, userChannelName,id}:VideoCartProp ) {
+
+    const router = useNavigate()
+
+
+    return <div className=" m-1 cursor-pointer w-100 h-75 overflow-hidden" onClick={() => router(`/watch?vid=${id}`)} >
+        <div className=" rounded-2xl overflow-hidden ">
+            <img src={thumbnailUrl} alt="thumbnail" />
+        </div>
+        <div className="content flex gap-5 mt-1">
+            <img src={userProfilePicture} alt="" className="w-10 h-10 rounded-[50%] " />
+            <div className="data">
+                <div className="title">{title}</div>
+                <div className="channel-name">{userChannelName}</div>
+            </div>
+
+        </div>
+
+    </div>
+}
+
+interface VideoCartProp {
+    id: string;
+    title: string;
+    description: string;
+    thumbnailUrl: string;
+    createdAt: string,
+    userid: string,
+    userChannelName: string,
+    userProfilePicture?: string
+}
---
+++ client/src/screen/Home.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { VideoCart } from "@/components/common/videoCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function Home() {
+
+    const [videos, setVideos] = useState<HomeVideo[]>([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/video")
+        .then( (res) => {
+            if ( res.status == 200 ) {
+
+                const { videos } = res.data
+                
+                setVideos(videos)
+                console.log("res data",res.data);
+            }
+            
+        })
+    }, [])
+
+    return (
+        <div className="">
+            {videos && videos?.map((video) => <VideoCart
+                id={video.id}
+                title={video.title}
+                description={video.description}
+                createdAt={video.createdAt}
+                thumbnailUrl={video.thumbnailUrl}
+                userid={video.user.id}
+                userChannelName={video.user.channelName}
+                userProfilePicture={video.user.profilePicture}
+            />)}
+        </div>
+    )
+}
+
+interface HomeVideo {
+    id: string;
+    title: string;
+    description: string;
+    thumbnailUrl: string;
+    createdAt: string
+    user: {
+        id: string,
+        channelName: string,
+        profilePicture: string
+    }
+}
