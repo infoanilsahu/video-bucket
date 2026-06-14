5ca0b9c132516408237fd7bb800c0f1caf07b7e8
infoanilsahu <info.anilsahu@gmail.com>
2026-06-14 21:56:03 +0530

    create(route): sign-in page

--- client/src/screen/signin.tsx
+++ client/src/screen/signin.tsx
@@ -1,12 +1,19 @@
 import { api } from "@/lib/api";
 import { useState } from "react"
+import { useNavigate } from "react-router";
 
 export function SignIn() {
 
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-    async function Submit() {
+    const router = useNavigate()
+
+    async function Submit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault() 
+        
+        if( username.length < 3 || password.length < 4 ) return;
+
         try {
             const res = await api({
                 method: "POST",
@@ -17,7 +24,9 @@ export function SignIn() {
             })
 
             if ( res.status === 200 ) {
+                console.log(res.data);
                 
+                router("/")
             }
 
         } catch (error: any) {
@@ -27,7 +36,19 @@ export function SignIn() {
 
     return (
         <>
-        
+        <form action="" onSubmit={Submit} className="m-2 ">
+            <input 
+            type="text" 
+            value={username} 
+            placeholder="User Name" 
+            onChange={(e) => setUsername(e.currentTarget.value)} />
+            <input 
+            type="text" 
+            value={password} 
+            placeholder="Password" 
+            onChange={(e) => setPassword(e.currentTarget.value)} />
+            <button type="submit" className="bg-black text-white px-4 font-semibold rounded-md cursor-pointer">Login</button>
+        </form>
         </>
     )
 }
