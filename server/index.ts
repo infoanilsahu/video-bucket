7bce96b9f9a4ca3db431449b33a70642706301ce
infoanilsahu <info.anilsahu@gmail.com>
2026-06-16 14:36:30 +0530

    fix(auth): add cookies option secure false

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,16 @@ import bodyParser from 'body-parser';
 import authRoutes from './api/auth';
 import videoRoutes from './api/video';
 import cookieParser from 'cookie-parser'
+import objectStore from './api/objectStore'
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3001",
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
